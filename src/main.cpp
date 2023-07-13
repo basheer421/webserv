2ee3e162aa2ee60987e91818e475561e377d5f45
basheer421 <bammar@student.42abudhabi.ae>
2023-07-13 21:08:31 +0400

    server setup

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 20:06:07 by bammar            #+#    #+#             */
-/*   Updated: 2023/07/13 21:06:34 by bammar           ###   ########.fr       */
+/*   Updated: 2023/07/13 21:07:25 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,24 @@
 
 int main(void)
 {
-    std::cout << "Server started\n";
+    struct sockaddr_in address;
+    const int PORT = 8080; //Where the clients can reach at
+    /* htonl converts a long integer (e.g. address) to a network representation */
+    /* htons converts a short integer (e.g. port) to a network representation */
+    memset((char *)&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(PORT);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
+    {
+        perror("cannot create socket");
+        return 0;
+    }
+    if (bind(server_fd,(struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        return 0;
+    }
     return (0);
 }
