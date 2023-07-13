09fb5082ac2ecdf44b63661b1c28cea50e95b026
basheer421 <bammar@student.42abudhabi.ae>
2023-07-13 21:32:12 +0400

    receiving a connection

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 20:07:27 by bammar            #+#    #+#             */
-/*   Updated: 2023/07/13 20:26:37 by bammar           ###   ########.fr       */
+/*   Updated: 2023/07/13 21:24:49 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,6 @@
 #include <sstream>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <unistd.h>
+
+# define PORT 8080
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 20:06:07 by bammar            #+#    #+#             */
-/*   Updated: 2023/07/13 21:07:25 by bammar           ###   ########.fr       */
+/*   Updated: 2023/07/13 21:27:01 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,51 @@
 int main(void)
 {
     struct sockaddr_in address;
-    const int PORT = 8080; //Where the clients can reach at
+    int addrlen = sizeof(address);
     /* htonl converts a long integer (e.g. address) to a network representation */
     /* htons converts a short integer (e.g. port) to a network representation */
-    memset((char *)&address, 0, sizeof(address));
+    bzero((char *)&address, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_port = htons(PORT);
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int server_fd = socket(address.sin_family, SOCK_STREAM, 0);
     if (server_fd < 0)
     {
-        perror("cannot create socket");
-        return 0;
+        std::cerr << "cannot create socket\n";
+        return (1);
     }
-    if (bind(server_fd,(struct sockaddr *)&address, sizeof(address)) < 0)
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
-        perror("bind failed");
-        return 0;
+        std::cerr << "bind failed\n";
+        return (1);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        std::cerr << "listen failed\n";
+        return (1);
+    }
+
+    while (true)
+    {
+        int client;
+
+        std::cout << "Waiting for a connection...\n";
+        client = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        if (client < 0)
+        {
+            std::cerr << "listen failed\n";
+            return (1);
+        }
+        char buffer[30000] = {0};
+        int read_res = read(client, buffer, 29999);
+        if (read_res < 0)
+        {
+            std::cerr << "read failed\n";
+            return (1);
+        }
+        std::string buffer_str = buffer;
+        std::cout << buffer_str << std::endl;
+        close(client);
     }
     return (0);
 }
